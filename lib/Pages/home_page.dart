efc74ece6111d8fe66d3a23bc857c87e483589d9
JulianD97 <julian.a97@hotmail.es>
2021-09-25 08:30:02 -0500

    commit

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -75,7 +75,33 @@ class _HomepageState extends State<Homepage> {
 Body() {
   return Scaffold(
     body: Center(
-      child: ElevatedButton(child: const Text('Log out'), onPressed: () {}),
-    ),
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(child: const Text('Log out'), onPressed: () {}),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Image.network(
+            'https://flutter.github.io/assets-for-api-docs/assets/widgets/puffin.jpg',
+            frameBuilder: (BuildContext context, Widget child, int? frame,
+                bool wasSynchronouslyLoaded) {
+              if (wasSynchronouslyLoaded) {
+                return child;
+              }
+              return AnimatedOpacity(
+                child: child,
+                opacity: frame == null ? 0 : 1,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeOut,
+              );
+            },
+          ),
+        )
+      ],
+    )),
   );
 }
